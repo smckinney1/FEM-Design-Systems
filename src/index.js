0f519cf09a2a91bd50b4ca3fb8160292a765398e
Shannon McKinney <13708207+smckinney1@users.noreply.github.com>
2022-10-15 15:57:46 -0400

    🚨 Shows what dark theme could start to look like, but feature incomplete.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 
-import { defaultTheme, GlobalStyle } from './utils';
+import { darkTheme, defaultTheme, GlobalStyle } from './utils';
 import {
   ErrorButton,
   PrimaryButton,
@@ -12,34 +12,48 @@ import {
   WarningButton,
 } from './components/Buttons.styled';
 
-const App = () => (
-  <main>
-    <PrimaryButton>Primary</PrimaryButton>
-    <SecondaryButton>Secondary</SecondaryButton>
-    <TertiaryButton>Tertiary</TertiaryButton>
-    <PrimaryButton disabled>Primary Disabled</PrimaryButton>
-    <SecondaryButton disabled>Secondary Disabled</SecondaryButton>
-    <TertiaryButton disabled>Tertiary</TertiaryButton>
+const App = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
 
-    <hr />
+  return (
+    <ThemeProvider theme={isDarkTheme ? darkTheme : defaultTheme}>
+      <GlobalStyle />
+      <header>
+        <button
+          onClick={() => setIsDarkTheme(!isDarkTheme)}
+          style={{ background: 'none', margin: '0 16px 24px', padding: '8px' }}
+        >
+          Toggle theme
+        </button>
+      </header>
+      <main
+        style={{ background: isDarkTheme ? defaultTheme.colors.primary : darkTheme.colors.primary }}
+      >
+        <PrimaryButton>Primary</PrimaryButton>
+        <SecondaryButton>Secondary</SecondaryButton>
+        <TertiaryButton>Tertiary</TertiaryButton>
+        <PrimaryButton disabled>Primary Disabled</PrimaryButton>
+        <SecondaryButton disabled>Secondary Disabled</SecondaryButton>
+        <TertiaryButton disabled>Tertiary</TertiaryButton>
 
-    <PrimaryButton size="small">Small button</PrimaryButton>
-    <PrimaryButton size="large">Large button</PrimaryButton>
+        <hr />
 
-    <hr />
+        <PrimaryButton size="small">Small button</PrimaryButton>
+        <PrimaryButton size="large">Large button</PrimaryButton>
 
-    <ErrorButton>Error button</ErrorButton>
-    <SuccessButton>Success button</SuccessButton>
-    <WarningButton>Warning button</WarningButton>
-  </main>
-);
+        <hr />
+
+        <ErrorButton>Error button</ErrorButton>
+        <SuccessButton>Success button</SuccessButton>
+        <WarningButton>Warning button</WarningButton>
+      </main>
+    </ThemeProvider>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
